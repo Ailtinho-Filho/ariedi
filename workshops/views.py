c37d4b878e36d55e654f3139fd75b4e69c5faf31
ariedi <joaoariedi@gmail.com>
2020-02-28 22:04:14 -0300

    workshop images debug

--- workshops/views.py
+++ workshops/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from workshops.models import Workshop, WorkshopImage
+from workshops.models import Workshop
 
 
 class WorkshopDetail(TemplateView):
@@ -9,9 +9,6 @@ class WorkshopDetail(TemplateView):
     template_name = "workshops/workshop.html"
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context['workshop'] = Workshop.objects.first()
-        context['workshop_images'] = WorkshopImage.objects.filter(workshop_id=context['workshop'].id)
         return context
